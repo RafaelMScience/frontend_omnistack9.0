696918fbea4ad5ab57d530e7e134b3d248e70542
RafaelM <RafaelM.Science@gmail.com>
2019-10-04 21:09:33 -0400

    feat web telas completas

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React from "react";
+
+import Login from "./pages/Login";
+import Dasboard from "./pages/Dashboard";
+import New from "./pages/New";
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Login} />
+        <Route path="/dashboard" component={Dasboard} />
+        <Route path="/new" component={New} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
